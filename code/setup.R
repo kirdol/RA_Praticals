2d65a963ac031e1a86c78ad5ece7d76efe6b4da8
kirdol <lodrik.adam@hotmail.ch>
2024-09-26 14:53:42 +0200

    subdivisions

---
+++ code/setup.R
@@ -0,0 +1,13 @@
+# load the required packages and install them if they are not.
+packages <- c(
+  "here"
+)
+
+# Function that install the packages if not already installed on your computer
+for (pkg in packages) {
+  if (!pkg %in% installed.packages()) {
+    install.packages(pkg)}}
+
+# load the packages
+for (pkg in packages) {
+  library(pkg, character.only = TRUE)}
